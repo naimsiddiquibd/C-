c97af088d9db332538f7c0cf34a06cb1fa65411f
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-10-18 19:20:46 +0600

    simple input added

---
+++ 02-simple-input.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int number1, number2, total;
+    scanf("%d %d", &number1, &number2);
+    total = number1 + number2;
+    printf("The total number is %d\n", total);
+    return 0;
+}
